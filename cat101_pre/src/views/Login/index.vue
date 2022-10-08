5ee45fd56f459df0f762fce229456b407b2b1f44
isSJa <2583845608@qq.com>
2022-10-08 14:06:43 +0800

    本地仓库保存sj

--- cat101_pre/src/views/Login/index.vue
+++ cat101_pre/src/views/Login/index.vue
@@ -1,23 +1,43 @@
 <template>
   <div>
-    Login.vue注册注册
-    <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+    Login.vue
+    <el-form :model="ruleForm"
+      status-icon
+      :rules="rules"
+      ref="ruleForm"
+      label-width="100px"
+      class="demo-ruleForm"
+    >
       <el-form-item label="密码" prop="pass">
-        <el-input type="password" v-model="ruleForm.pass" autocomplete="off"></el-input>
+        <el-input
+          type="password"
+          v-model="ruleForm.pass"
+          autocomplete="off"
+        ></el-input>
       </el-form-item>
       <el-form-item label="确认密码" prop="checkPass">
-        <el-input type="password" v-model="ruleForm.checkPass" autocomplete="off"></el-input>
+        <el-input
+          type="password"
+          v-model="ruleForm.checkPass"
+          autocomplete="off"
+        ></el-input>
       </el-form-item>
       <el-form-item label="年龄" prop="age">
         <el-input v-model.number="ruleForm.age"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
+        <el-button type="primary" @click="submitForm('ruleForm')"
+          >提交</el-button
+        >
         <el-button @click="resetForm('ruleForm')">重置</el-button>
       </el-form-item>
     </el-form>
-    <el-button type="warning" plain @click="$router.push('/layout/user')">用户登录</el-button>
-    <el-button type="warning" plain @click="$router.push('/layout/admin')">管理员登录</el-button>
+    <el-button type="warning" plain @click="$router.push('/layout/user')"
+      >用户登录</el-button
+    >
+    <el-button type="warning" plain @click="$router.push('/layout/admin')"
+      >管理员登录</el-button
+    >
   </div>
 </template>
 
@@ -91,6 +111,4 @@ export default {
 };
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
