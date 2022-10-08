8769a95aa7b5453816530654561ac8af05f9b02c
Stitch <2678359340@qq.com>
2022-10-08 18:52:50 +0800

    头部按钮的显示问题解决

--- cat101_pre/src/main.js
+++ cat101_pre/src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router/index'
+import store from "@/store";
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
 import axios from 'axios'
@@ -14,5 +15,6 @@ Vue.prototype.$axios = axios
 
 new Vue({
   render: h => h(App),
+  store,
   router,
 }).$mount('#app')
