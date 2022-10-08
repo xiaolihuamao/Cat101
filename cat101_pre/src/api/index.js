fa617f710533a493562b5067f3120e7a1d0397a1
Stitch <2678359340@qq.com>
2022-10-08 17:33:52 +0800

    fix-三人第一次合并

--- cat101_pre/src/api/index.js
+++ cat101_pre/src/api/index.js
@@ -1,6 +1,6 @@
-
 // 封装接口方法
 import request from "@/utils/request";   //引入自定义的axios函数
+
 
 
 //登录请求接口，定义注册按钮点击后，向后端发送请求的函数，参数是表单提交的数据
@@ -17,21 +17,19 @@ import request from "@/utils/request";   //引入自定义的axios函数
  * @param {*} Uinfo 
  * @returns 
  */
-export const registerAPI = ({ Uname, Upwd, Uxname,Usex,Utel,Usite,Utype,Usrl,Uinfo }) => {
+//  JSON.stringify({ uname, upwd, uxname,usex,utel,usite,utype })
+export const registerAPI = (json) => {     //,usrl,uinfo
     return request({
-        url: '',       //注册接口地址：以向该接口发送注册表单的数据，放入用户信息表获得返回结果           跟后端沟通！！！！！！！
+        // headers: {
+        //     'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' 
+        //   },
+        url: '/user/register',       //注册接口地址：以向该接口发送注册表单的数据，放入用户信息表获得返回结果           跟后端沟通！！！！！！！
         method: 'POST',
-        data: {
-            Uname, 
-            Upwd,
-            Uxname,
-            Usex,
-            Utel,
-            Usite,
-            Utype,
-            Usrl,
-            Uinfo
-        }
+        data: json,
+        
+        // header:{
+        //     'Content-Type':'application/json'  //如果写成contentType会报错
+        // }
     
     })
 }
@@ -44,16 +42,11 @@ export const registerAPI = ({ Uname, Upwd, Uxname,Usex,Utel,Usite,Utype,Usrl,Uin
  * @returns 返回一个promise对象
  */
 
-export const loginAPI = ({} ) => {
-export const loginAPI = ({} ) => {
+export const loginAPI = (json) => {
     return request ({
-        url:'',     //登录接口地址：以向该接口发送登录表单的数据，与用户信息表比较然后返回结果      跟后端沟通！！！！！！！
+        url:'/user/login',     //登录接口地址：以向该接口发送登录表单的数据，与用户信息表比较然后返回结果      跟后端沟通！！！！！！！
         method: 'POST', 
-        data: {
-            Uname,
-            Upwd,
-            Utype
-        }
+        data: json
     })
 }
 
--- cat101_pre/src/components/welLogo.vue
+++ cat101_pre/src/components/welLogo.vue
@@ -3,34 +3,32 @@
   <div class="header-g">
     <!-- logo图片 -->
     <div class="logo" @click="$router.push('/layout/user')">
-      <img src="../assets/img/logo-1.png" alt="LOGO"/>
+      <img src="../assets/img/logo-1.png" alt="LOGO" />
     </div>
     <!-- 欢迎语
        <div>可爱的猫猫在等你抱回家哦！</div> -->
 
     <!-- 右边的消息和头像以及设置按钮 -->
     <div class="block">
-
-      <!--TODO：以下代码需要进行修改-->
-      <!--登录注册按钮  注意：登录后不显示 v-if="false"-->
-      <div class="login">
+      <!-- 登录注册按钮  注意：登录后不显示 v-if="false"-->
+      <div class="login"  v-if="$store.state.isLogin">
         <el-button
-            size="small"
-            round
-            @click="$router.push('/layout/register')"
-            style="margin-left: 30px"
-        >注册
-        </el-button>
-        <el-button size="small" round @click="$router.push('/layout/login')">登录
-        </el-button>
+          size="small"
+          round
+          @click="$router.push('/layout/register')"
+          style="margin-left: 30px"
+          >注册</el-button
+        >
+        <el-button size="small" round @click="$router.push('/layout/login')"
+          >登录</el-button
+        >
       </div>
       <!-- 新消息显示按钮，可跳转到另一个消息页面   注意：登录前不显示 v-if="false"-->
-      <div style="float: right; width: 84px; height: 40px">
+      <div style="float: right; width: 84px; height: 40px" v-if="$store.state.isNew" >
         <el-badge :value="2" class="item" type="warning">
           <el-button size="large">新消息</el-button>
         </el-badge>
       </div>
-
       <!-- 用户头像图标 -->
       <div style="float: right; margin-right: 30px; margin-top: -10px">
         <el-avatar icon="el-icon-user-solid" :size="50"></el-avatar>
@@ -44,9 +42,9 @@ export default {
   data() {
     return {
       circleUrl:
-          "https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png",
+        "https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png",
       squareUrl:
-          "https://cube.elemecdn.com/9/c2/f0ee8a3c7c9638a54940382568c9dpng.png",
+        "https://cube.elemecdn.com/9/c2/f0ee8a3c7c9638a54940382568c9dpng.png",
       sizeList: ["large", "medium", "small"],
     };
   },
@@ -60,18 +58,15 @@ export default {
   border: 1px solid #eee;
   border-bottom: 0;
 }
-
 .logo {
   float: left;
   height: 100px;
   margin-left: 40px;
   margin-top: 10px;
 }
-
 .logo img {
   height: 100%;
 }
-
 .block {
   float: right;
   height: 70px;
@@ -79,12 +74,10 @@ export default {
   margin-top: 50px;
   margin-right: 40px;
 }
-
 .login {
   float: right;
   width: 152px;
   height: 32px;
   margin-top: 4px;
-  cursor: pointer;
 }
 </style>
